e39a7dc9f501d3005aaf2615587ed6839de814dd
Francisco Fischer <francisco.fischer92@gmail.com>
2023-04-26 17:36:05 -0300

    task list component implemented

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../interfaces/task.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks$: Observable<Task[]>;
 
+  constructor(private taskService: TaskService) {
+    this.tasks$ = this.taskService.getTasks();
+  }
+
+  ngOnInit(): void {}
+
+  toggleTask(task: Task) {
+    task.completed = !task.completed;
+    this.taskService.updateTask(task);
+  }
 }
