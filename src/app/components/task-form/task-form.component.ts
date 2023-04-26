6c578d0a0d9651fe476e353ade7e26c3b5903181
Francisco Fischer <francisco.fischer92@gmail.com>
2023-04-26 17:33:15 -0300

    task form component implementation

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../interfaces/task.interface';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.css']
 })
-export class TaskFormComponent {
+export class TaskFormComponent implements OnInit {
+  taskForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private taskService: TaskService) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      const task: Task = {
+        id: Date.now(),
+        title: this.taskForm.get('title')?.value,
+        completed: false
+      };
+      this.taskService.addTask(task);
+      this.taskForm.reset();
+    }
+  }
 }
